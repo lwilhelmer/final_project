b3aa4f8c5ef3509ff154f7b9676d821685f2a5c4
Lea Wilhelmer <lea.wilhelmer@sjsu.edu>
2025-12-04 12:08:06 -0800

    working map and pins

--- sketch.js
+++ sketch.js
@@ -2,10 +2,3 @@ function setup() {
   let canvas = createCanvas(900, 500);
   canvas.parent("map-container");
 }
-
-function draw() {
-  background(220);
-  textSize(32);
-  fill(0);
-  text("This can become your custom illustrated map", 20, 50);
-}
